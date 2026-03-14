7f19f6432597aa90684a9a873a6f69fe934a2fb2
Vegar <vegar@epost.no>
2026-03-14 16:11:59 +0100

    Fix analytics script, update domain to costofliving.no and add CNAME

--- docs/assets/javascripts/analytics.js
+++ docs/assets/javascripts/analytics.js
@@ -1,9 +1,10 @@
-<!-- Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-KCYT87ZMVZ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+// Google Analytics
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-KCYT87ZMVZ';
+document.head.appendChild(script);
 
-  gtag('config', 'G-KCYT87ZMVZ');
-</script>
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-KCYT87ZMVZ');
